ca37739c2bbd40a73a3d57774ccb3162590d00c5
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-08 22:12:48 -0500

    init

---
+++ ninja.js
@@ -0,0 +1,22 @@
+class Ninja{
+    constructor(name, health, speed = 3, strength = 3){
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName = () => {
+        console.log(this.name)
+    }
+    showStats = () => {
+        console.log(`Ninja: ${this.name} Health: ${this.health} Speed: ${this.speed} Strength: ${this.speed}`)
+    }
+    drinkSake = () => {this.health += 10}
+}
+
+const Roby = new Ninja("Roby", 100, 10, 10);
+Roby.sayName();
+Roby.showStats(); 
+Roby.drinkSake();
+Roby.showStats(); 
